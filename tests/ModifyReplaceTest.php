80cbf0c078ccec5f56f3d4c72885360f7443d589
Dave Medema <davemedema@gmail.com>
2019-05-08 09:13:08 -0500

    make sure unit tests sanitize

--- tests/ModifyReplaceTest.php
+++ tests/ModifyReplaceTest.php
@@ -16,50 +16,57 @@ class ModifyReplaceTest extends AbstractTestCase
     // Default...
     $sortie = new Sortie("[foo->replace]");
 
-    $actual = $sortie->process(['foo' => 'Foo 123 Bar 123 Baz']);
+    $actual = $sortie->process(['foo' => 'Foo 123 Bar 123 Baz'], true);
 
     $this->assertSame('Foo 123 Bar 123 Baz', $actual);
 
     // Basic...
     $sortie = new Sortie("[foo->replace:'/\d+/':'000']");
 
-    $actual = $sortie->process(['foo' => 'Foo 123 Bar 123 Baz']);
+    $actual = $sortie->process(['foo' => 'Foo 123 Bar 123 Baz'], true);
 
     $this->assertSame('Foo 000 Bar 000 Baz', $actual);
 
     // Complex...
     $sortie = new Sortie("http://[foo->replace:'/\/$/':'']/bar.html");
 
-    $actual = $sortie->process(['foo' => 'foo.com/']);
+    $actual = $sortie->process(['foo' => 'foo.com/'], true);
 
     $this->assertSame('http://foo.com/bar.html', $actual);
 
     // Brackets...
     $sortie = new Sortie("[foo->replace:'/%LB%f%RB%oo/':'FOO']");
 
-    $actual = $sortie->process(['foo' => 'foo bar baz']);
+    $actual = $sortie->process(['foo' => 'foo bar baz'], true);
 
     $this->assertSame('FOO bar baz', $actual);
 
     // Colon
     $sortie = new Sortie("[foo->replace:'/%CN%/':'FOO']");
 
-    $actual = $sortie->process(['foo' => 'foo : baz']);
+    $actual = $sortie->process(['foo' => 'foo : baz'], true);
 
     $this->assertSame('foo FOO baz', $actual);
 
     // Asterisks
     $sortie = new Sortie("[foo->replace:'/\*+/':'']");
 
-    $actual = $sortie->process(['foo' => '*foo*']);
+    $actual = $sortie->process(['foo' => '*foo*'], true);
 
     $this->assertSame('foo', $actual);
 
     // Subpattern
     $sortie = new Sortie('[foo->replace:\'/^Foo\s(123)\sBar\s(456).*$/\':\'${1}-${2}\']');
 
-    $actual = $sortie->process(['foo' => 'Foo 123 Bar 456 Baz']);
+    $actual = $sortie->process(['foo' => 'Foo 123 Bar 456 Baz'], true);
 
     $this->assertSame('123-456', $actual);
+
+    // Real-World Example
+    $sortie = new Sortie('[WebsiteVDPURL->replace:\'#carsforsale\.com/vehicle/details#i\':\'carwizedetroit.com/Inventory/Details\']');
+
+    $actual = $sortie->process(['WebsiteVDPURL' => 'https://www.carsforsale.com/vehicle/details/foo/bar/'], true);
+
+    $this->assertSame('https://www.carwizedetroit.com/Inventory/Details/foo/bar/', $actual);
   }
 }
